ba85656bd3c4e54074a70dcfbcaf6bd084573b74
Kristina <kr.hranovska@gmail.com>
2023-11-14 01:15:01 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+const feedbackForm = document.querySelector('.feedback-form');
+const formData = {};
+
+// const localInfo = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+// if (localInfo) {
+//     feedbackForm.elements.email.value = localInfo.email;
+//     feedbackForm.elements.message.value = localInfo.message;
+// }
+
+const localInfo = JSON.parse(localStorage.getItem("feedback-form-state")) ?? {};
+feedbackForm.elements.email.value = localInfo.email || '';
+feedbackForm.elements.message.value = localInfo.message || '';
+
+feedbackForm.addEventListener('input', sendUserInfo);
+
+function sendUserInfo(event) {
+    const { name, value } = event.target;
+
+    if (name === 'email' || name === 'message') {
+        formData[name] = value;
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+    }
+}
 
