2610017946cde1b53a68b683403faacfb2a3e537
Kristina <kr.hranovska@gmail.com>
2023-11-13 21:48:07 +0200

    task 2 is ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', _.throttle(playOn, 1000));
+
+function playOn(date) {
+    const { seconds } = date;
+    localStorage.setItem("videoplayer-current-time", seconds);
+}
+const localVideoTime = localStorage.getItem("videoplayer-current-time");
+
+if (localVideoTime !== null) {
+    player.setCurrentTime(localVideoTime);
+}
