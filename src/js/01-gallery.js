ef030dcde7ae629843d502c22a03ed576c884de9
Kristina <kr.hranovska@gmail.com>
2023-11-13 18:21:17 +0200

    1 task is ready

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,52 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import * as basicLightbox from 'basiclightbox';
+import "basiclightbox/dist/basicLightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', onClickOpenImg);
+
+const imgItems = galleryItems.map(({ preview, original, description }) =>
+    ` <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                width='360'
+            />
+        </a>
+    </li>`
+).join('');
+
+gallery.insertAdjacentHTML('beforeend', imgItems);
+
+function onClickOpenImg(event) {
+    event.preventDefault();
+
+    const { target } = event;
+
+    if (!target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+        `<img src="${target.dataset.source}" alt="${target.alt}" width="1280">`
+    )
+    instance.show();
+
+    const keyPress = event => {
+        if (event.code === 'Escape') {
+            instance.close();
+            document.removeEventListener('keydown', keyPress);
+        }
+    };
+
+    if (instance.show()) {
+        document.addEventListener('keydown', keyPress);
+    }
+
+}
