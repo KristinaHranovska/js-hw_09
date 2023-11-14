1d90313a278d9358100a48aee508028b9bdb280f
Kristina <kr.hranovska@gmail.com>
2023-11-14 21:43:05 +0200

    task 3 is ready

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,12 +9,16 @@ feedbackForm.elements.message.value = localInfo.message || '';
 feedbackForm.addEventListener('input', fieldUserInfo);
 feedbackForm.addEventListener('submit', sendUserInfo);
 
+const updateLocalStorage = _.throttle(() => {
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}, 500);
+
 function fieldUserInfo(event) {
     const { name, value } = event.target;
 
     if (name === 'email' || name === 'message') {
         formData[name] = value;
-        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+        updateLocalStorage();
     }
 }
 
